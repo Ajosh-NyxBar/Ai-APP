41a91b49ada55f5310a212413f0588a58693dceb
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-09-13 11:08:12 +0700

    finish code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Features from "./components/Features";
 import WorkFlow from "./components/WorkFlow";
 import SubcribePricing from "./components/SubcribePricing";
 import Footer from "./components/footer";
+import Testimonials from "./components/Testimonials";
 const App = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const App = () => {
         <Features />
         <WorkFlow />
         <SubcribePricing />
+        <Testimonials />
         <Footer />
       </div>
     </>
